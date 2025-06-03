bd194a598ea9dc328bac358bf269fe9c105e1b9d
Matvey <matpalion@mail.ru>
2025-06-03 13:43:42 -0200

    итог

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,24 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+function createCard(cardData) {
+  const cardElement = document.querySelector('#card-template').content.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const removeButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = cardData.link;
+  cardTitle.textContent = cardData.name;
+  
+  removeButton.addEventListener('click', function() {
+    cardElement.remove(); 
+  });
+  
+  return cardElement;
+}
+const placesList = document.querySelector('.places__list');
+initialCards.forEach(cardData => {
+  const cardPart = createCard(cardData, function() {
+  });
+  placesList.appendChild(cardPart);
+});
